7338277f122c60b5a15a6e3034565ef4bf54fe32
Maksick <mezhow@ya.ru>
2024-10-13 19:22:45 +0500

    2 commit

--- bin/freezed_test_project.dart
+++ bin/freezed_test_project.dart
@@ -3,5 +3,6 @@ import 'package:freezed_test_project/provider/fake_api_provider.dart';
 void main(List<String> arguments) async {
   final provider = FakeApiProvider();
   final carts = await provider.getCarts();
+  
   print(carts);
 }
